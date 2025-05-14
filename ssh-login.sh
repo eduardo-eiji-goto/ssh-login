ba7238dd83ba9685a540db8f6b318c805fbaef39
eduardo-e-goto <eduardo@feraweb.com.br>
2025-05-14 15:12:28 -0300

    feat: functioning script

---
+++ ssh-login.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "No input provided"
+    exit 1
+fi
+
+if [ -d "$1" ]; then
+    {
+        read -r username
+        read -r host
+    } < "$1/credentials.txt"
+
+    sshpass -f$1/password.txt ssh $username@$host
+else
+    echo "Input should be a Directory"
+    exit 1
+fi
+
