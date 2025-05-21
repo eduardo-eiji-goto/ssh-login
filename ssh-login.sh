22cdf0d01542854704d1779999ec569702f6361a
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-05-21 14:10:13 -0300

    feat: improved argument validation

--- ssh-login.sh
+++ ssh-login.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
+#
+# Simple script to facilitate connecting to ssh using password
 
-# Checks if there's an input
 if [ -z "$1" ]; then
-    echo "No input provided"
+    echo "No argument provided"
     exit 1
 fi
 
-if [ -d "$1" ]; then
-    {
-        read -r username
-        read -r host
-    } < "$1/credentials.txt"
-
-    sshpass -f $1/password.txt ssh $username@$host
-else
-    echo "Input should be a Directory"
+if [ $# -gt 1 ]
+then
+    echo "Expected only 1 argument"
     exit 1
 fi
+
+if [ ! -d "$1" ]; then
+    echo "Argument input should be a directory"
+    exit 1
+fi
+
+if [ ! -f "$1/credentials.txt" ]; then
+    echo "Credentials file not found in directory"
+    exit 1
+fi
+
+if [ ! -f "$1/password.txt" ]; then
+    echo "Password file not found in directory"
+    exit 1
+fi
+
+{
+    read -r username
+    read -r host
+} < "$1/credentials.txt"
+
+sshpass -f $1/password.txt ssh $username@$host
 
